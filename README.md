e6f78afa87151e23d21c2d7004906bc60efd0265
Jeremy Marin <marin.jeremy@gmail.com>
2018-04-04 15:48:48 +0900

    Complete Skyline

--- README.md
+++ README.md
@@ -10,4 +10,8 @@
 
 [Aquarium](http://grosgg.github.io/screensavers/aquarium)
 
+## City Skyline
+
+[City Skyline](http://grosgg.github.io/screensavers/skyline)
+
 Ocean life SVGs by Yu luck from the Noun Project
--- js/skyline.js
+++ js/skyline.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+  // Big Building Windows
   const bbWindows = Array.prototype.slice.call(document.getElementsByClassName('big-building-window'));
   console.log(bbWindows);
   bbWindows.forEach(function(bbWindow, index) {
@@ -13,4 +14,53 @@ document.addEventListener("DOMContentLoaded", function(event) {
       }
     }
   });
+
+  // Small Building Windows
+  const sbWindows = Array.prototype.slice.call(document.getElementsByClassName('small-building-window'));
+  console.log(sbWindows);
+  sbWindows.forEach(function(sbWindow, index) {
+    for (let y = 1; y <= 9; y++) {
+      for (let x = 0; x < 6; x++) {
+        clone = sbWindow.cloneNode(false);
+        clone.style.top = `${y * 15}px`;
+        clone.style.left = `${10 + (x * 31)}px`;
+        clone.style.display = 'block';
+        clone.style.background = ['#ffff99', '#000066'][Math.floor(Math.random() * 2)]
+        sbWindow.parentNode.appendChild(clone);
+      }
+    }
+  });
+
+  // Medium Building Windows
+  const mbWindows = Array.prototype.slice.call(document.getElementsByClassName('medium-building-window'));
+  console.log(mbWindows);
+  mbWindows.forEach(function(mbWindow, index) {
+    for (let y = 1; y <= 13; y++) {
+      for (let x = 0; x < 10; x++) {
+        clone = mbWindow.cloneNode(false);
+        clone.style.top = `${y * 15}px`;
+        clone.style.left = `${10 + (x * 16)}px`;
+        clone.style.display = 'block';
+        clone.style.background = ['#ffffcc', '#000066'][Math.floor(Math.random() * 2)]
+        mbWindow.parentNode.appendChild(clone);
+      }
+    }
+  });
+
+  // Love Hotel Windows
+  const lhWindows = Array.prototype.slice.call(document.getElementsByClassName('love-hotel-window'));
+  console.log(lhWindows);
+  lhWindows.forEach(function(lhWindow, index) {
+    for (let y = 1; y <= 13; y++) {
+      for (let x = 0; x < 7; x++) {
+        clone = lhWindow.cloneNode(false);
+        clone.style.top = `${60 + (y * 15)}px`;
+        clone.style.left = `${10 + (x * 22)}px`;
+        clone.style.display = 'block';
+        clone.style.background = ['#eeeecc', '#000066'][Math.floor(Math.random() * 2)]
+        lhWindow.parentNode.appendChild(clone);
+      }
+    }
+  });
+
 });
