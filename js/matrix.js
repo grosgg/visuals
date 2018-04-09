09db80911513d677e85827bbac33c8ac3b485cba
Jeremy Marin <marin.jeremy@gmail.com>
2018-04-09 15:47:00 +0900

    Add presets + randomize

--- js/matrix.js
+++ js/matrix.js
@@ -22,9 +22,9 @@ function setField(fieldId) {
   let dots;
   let clone;
   const field = document.getElementById(fieldId);
-  console.log(fieldId);
-  console.log(field);
-  console.log(field.value); 
+  // console.log(fieldId);
+  // console.log(field);
+  // console.log(field.value); 
   switch (fieldId) {
     case 'size':
       dots = Array.prototype.slice.call(document.getElementsByClassName('dot'));
@@ -52,4 +52,30 @@ function setCircle(active) {
   } else {
     dots.forEach(function(dot) { dot.classList.remove("circle"); });
   }
+}
+
+function pickPreset(preset) {
+  switch(preset) {
+    case 'terminal':
+      setPreset('#246E1D', '#000000', '20', 'square');
+      break;
+    case 'bubbletea':
+      setPreset('#B27CDB', '#FFF6C9', '40', 'circle');
+      break;
+    case 'random':
+      setPreset(
+        '#'+(Math.random()*0xFFFFFF<<0).toString(16),
+        '#'+(Math.random()*0xFFFFFF<<0).toString(16),
+        Math.random() * (100 - 20) + 20,
+        ['square', 'circle'][Math.floor(Math.random() * 2)]
+      );
+      break;
+  }
+}
+
+function setPreset(color1, color2, size, shape) {
+  document.getElementById('color1').value = color1; setField('color1');
+  document.getElementById('color2').value = color2; setField('color2');
+  document.getElementById('size').value = size; setField('size');
+  document.getElementById(shape).checked = true; setCircle(shape == 'circle');
 }
