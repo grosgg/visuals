874a6d3ec5c3a6e4c61e3954673a4f975b5775b9
Jeremy Marin <marin.jeremy@gmail.com>
2021-02-14 02:01:35 +0900

    Improve mountain generation

--- recluse/index.js
+++ recluse/index.js
@@ -6,7 +6,7 @@ function draw() {
   noLoop();
   background(100, 0, 220, 150);
   
-  for(let i = 0; i < 4; i++) {
+  for(let i = 0; i < 5; i++) {
     let m = new Mountain(i);
     m.show();
   }
@@ -14,21 +14,25 @@ function draw() {
 
 class Mountain {
   constructor(layer) {
-    this.baseHeight = random((layer+1) * height/6, (layer+2) * height/6);
-    this.amplitude = random(10, 50);
-    this.sharpness = random(30, 100);
+    // this.baseHeight = random((layer+1) * height/8, (layer+2) * height/8);
+    this.baseHeight = height/7 * (layer+1);
+    // this.amplitude = random(height/20*(layer+1)/2, height/4*(layer+1)/2);
+    this.amplitude = height/15 * (layer+1) * random(-1,1);
+    this.sharpness = random(height/4, height/(8 - layer));
+    // this.sharpness = random(height/2 * (layer+1), height/6);
     this.layer = layer;
   }
   
   show() {
     for(let x = 0; x < width; x++) {
       let bh = this.baseHeight;
-      let y = sin(x / this.sharpness) * this.amplitude;
-      let n = map(noise(x), 0, 1, -10 , 10);
+      let y1 = (sin(x / this.sharpness)) * this.amplitude;
+      let n1 = map(noise(x / this.sharpness * (this.layer+1)), 0, 1, 0, this.amplitude/4);
+      let n2 = map(noise(x), 0, 1, -5-this.layer , 5+this.layer);
 
       strokeWeight(2);
-      stroke(180 - this.layer*40, 100, 70);
-      line(x, bh+y+n, x, height);
+      stroke(180 - this.layer*25, 70 - this.layer*10, 0);
+      line(x, bh+y1+n1+n2, x, height);
     }
   }
 }
